294fb3a8f1d850dd588240dd7f878d57d99622f4
sonalcm <sonal.cmendis@gmail.com>
2018-05-07 19:37:04 +0530

    bunch o tweaks

--- js/main.js
+++ js/main.js
@@ -80,32 +80,30 @@ HOME PAGE VIDEO & HOME PAGE PRELOADER
   if (viewport.width >= 1030 && $(".main-container.home").length > 0) {
     var videoString = '<video width="320" preload="auto" id="introVideo" height="240" muted loop autoplay="autoplay"> <source src="video/video.mp4" type="video/mp4"> Your browser does not support the video tag. </video>';
 
-      $(".splash").prepend(videoString);
-      var video = document.getElementById("introVideo");
-      video.addEventListener(
-        "loadeddata",
-        function () {
-          $(".preloader").addClass("loaded");
-          setTimeout(() => {
-            $(".preloader").hide();
-          }, 500);
+    $(".splash").prepend(videoString);
+    var video = document.getElementById("introVideo");
+    video.addEventListener("loadeddata", function () {
+        $(".preloader").addClass("loaded");
+        setTimeout(() => {
+          $(".preloader").hide();
+        }, 500);
 
-          setTimeout(() => {
-            $(".splash-inner-container").css({
-              opacity: "1"
-            });
-          }, 1000);
+        setTimeout(() => {
+          $(".splash-inner-container").css({
+            opacity: "1"
+          });
+        }, 1000);
 
-          $(".down-arrows").fadeIn(1000);
-        },
-        false
-      );
-    
-  } else if ($(".main-container.home").length > 0){
-              $(".preloader").addClass("loaded");
-          setTimeout(() => {
-            $(".preloader").hide();
-          }, 500);
+        $(".down-arrows").fadeIn(1000);
+      },
+      false
+    );
+
+  } else if ($(".main-container.home").length > 0) {
+    $(".preloader").addClass("loaded");
+    setTimeout(() => {
+      $(".preloader").hide();
+    }, 500);
   }
 
   /****
